ee70d332132ae008869f0efc26d2a2de195c2033
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-05-31 21:33:02 +0900

    ✨ console.log 방지하는 규칙 추가 (#20)

--- packages/base/index.js
+++ packages/base/index.js
@@ -5,5 +5,15 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(eslint.configs.recommended, eslintPluginPrettierRecommended, ...tseslint.configs.recommended, {
   plugins: { 'simple-import-sort': simpleImportSort },
-  rules: { 'simple-import-sort/imports': 'error' },
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.object.name='console'][callee.property.name='log']",
+        message:
+          'console.log() 가 main 에 머지되는 것은 실수일 수 있습니다. 의도하지 않았다면 제거하고, 의도했다면 console.debug() 등 다른 메서드를 사용해주세요.',
+      },
+    ],
+  },
 });
