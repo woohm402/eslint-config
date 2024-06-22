13a768f43d0f748368da90e7038d774f67b7b503
woohm402 <woohm404@gmail.com>
2024-06-23 02:50:10 +0900

    printWidth 80으로 변경

--- packages/react/__tests__/wrong.tsx
+++ packages/react/__tests__/wrong.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
-const fetchData = async (name: string) => new Promise<string>((resolve) => setTimeout(() => resolve(name), 1000));
+const fetchData = async (name: string) =>
+  new Promise<string>((resolve) => setTimeout(() => resolve(name), 1000));
 
 export const Component = ({ name }: { name: string }) => {
   const [data, setData] = useState<string>();
