704f6fe161417e90c10cc3f094f1f0506d045ef8
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-06-26 21:48:36 +0900

    ✨ base v0.3.0: add no-shadow rule, use tseslint strict preset (#38)
    resolved #32

--- packages/base/__tests__/wrong.ts
+++ packages/base/__tests__/wrong.ts
@@ -3,3 +3,14 @@ import eslint from 'eslint';
 
 // eslint-disable-next-line no-console
 console.log({ eslint, typescript });
+
+const a = 1;
+
+function b() {
+  const a = 2;
+  return a;
+}
+
+// below is correct
+console.debug(a);
+b();
