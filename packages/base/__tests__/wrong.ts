f04731ca721586d0ec04f18fcf3d49da5240bda4
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-07-23 01:15:57 +0900

    ✨ add support restrict-template-expressions & v0.4.0 (#47)
    resolved #46

--- packages/base/__tests__/wrong.ts
+++ packages/base/__tests__/wrong.ts
@@ -1,16 +1,19 @@
 import typescript from 'typescript';
 import eslint from 'eslint';
 
-// eslint-disable-next-line no-console
-console.log({ eslint, typescript });
-
+// eslint-disable-next-line
 const a = 1;
+
+console.log({ eslint, typescript });
 
 function b() {
   const a = 2;
   return a;
 }
 
+const c: number = 1;
+const d = `aa ${c || null}`;
+
 // below is correct
-console.debug(a);
+console.debug(a, d);
 b();
