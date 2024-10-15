910efc2d122ae5b2ac78b693ec5130a32ca2d8e7
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-10-15 17:01:18 +0900

    💥 upgrade dependencies, base@0.9.0, react@0.8.0 (#64)

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,17 +1,16 @@
 import base from '@woohm402/eslint-config-base';
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
-import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
-import { fixupPluginRules, fixupConfigRules } from '@eslint/compat';
+import reactHooks from 'eslint-plugin-react-hooks';
+import react from 'eslint-plugin-react';
 
 export default (options: Parameters<typeof base>[0]) => [
   ...base(options),
-  ...fixupConfigRules(reactRecommended),
+  react.configs.flat.recommended,
   {
     plugins: {
-      'react-hooks': fixupPluginRules(eslintPluginReactHooks),
+      'react-hooks': reactHooks,
     },
     rules: {
-      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       'react-hooks/exhaustive-deps': 'error',
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
